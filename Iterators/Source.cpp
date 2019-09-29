ab2bb7d22a3ef4b369126b8ef1eb999c68db476d
rvdw <rogerscode@gmail.com>
2019-09-29 19:33:41 +0200

    Cleaned up solution to first assignment

--- Iterators/Source.cpp
+++ Iterators/Source.cpp
@@ -54,7 +54,6 @@ private:
 	int x;
 	int y;
 };
-
 void T15stuff()
 {
 	std::vector<int> vi = { 0,    1,    2,    3,    4,     5,    6 };
@@ -97,7 +96,6 @@ void T15stuff()
 	printI(std::begin(a), std::end(a));
 	*/
 }
-
 void Print(std::vector<Element>& evec)
 {
 	for (Element& e : evec)
@@ -106,7 +104,6 @@ void Print(std::vector<Element>& evec)
 	}
 	std::cout << "-------------------------" << std::endl;
 }
-
 void T16stuff()
 {
 	std::vector<Element> elementen = { {1,2,2},{4,2,12},{3,0,1},{0,0,0}, {99,4,11} };
@@ -186,24 +183,6 @@ struct Pube
 	{
 		return num;
 	}
-/*	class Sort1
-	{
-	public:
-		bool operator()(const Pube& lhs, const Pube& rhs) const
-		{
-			return lhs.num > rhs.num;
-		}
-	};
-*/
-/*	class Sort2
-	{
-	public:
-		bool operator()(const Pube& lhs, const Pube& rhs) const
-		{
-			return lhs.str < rhs.str;
-		}
-	};
-*/
 };
 
 // write your remove_erase_if template function here!
@@ -211,15 +190,12 @@ struct Pube
 // write your custom insertion operator here!
 std::ostream& operator<<(std::ostream& os, const Pube& p)
 {
-	os << p.num << ", " << p.str;
+	os << p.num << ", " << p.str ;
 	return os;
 }
 
 int main()
 {
-	//T15stuff();
-	//T16stuff();
-
 	////////////////////////////////////////////////////////////////////////////
 	//     !! HOMEWORK !!
 	// use the C++ <algorithms> as extensively as you can for these problems!
@@ -258,9 +234,9 @@ int main()
 	// output sorted meme list w/ score and name using custom insertion operator
 	std::cout << "<< Sort Memes >>" << std::endl;
 	{
-		// 1. Create vector with 4x elements of memes
-		std::vector<Pube> sortedMemes(4 * memes.size());// , { 0,"" });
+		// STEP 1. Create vector with 4x elements of memes
 		/*// Method 1	
+		std::vector<Pube> sortedMemes(4 * memes.size());// , { 0,"" });
 		int offset = memes.size();
 		auto insert2 = sortedMemes.begin();
 		for (auto in1 = memes.begin(), end1 = memes.end(); in1 != end1; ++in1, ++insert2)
@@ -269,31 +245,44 @@ int main()
 		}
 		*/
 		/*// Method 2
+		std::vector<Pube> sortedMemes(4 * memes.size());// , { 0,"" });
 		for (int i = 0; i < 4; i++)
 		{
 			int offset = i * memes.size();
 			std::copy(memes.begin(), memes.end(), sortedMemes.begin()+offset);
 		}
 		*/
-		// Method 3
+		/*// Method 3
+		std::vector<Pube> sortedMemes(4 * memes.size());// , { 0,"" });
 		for (size_t offset = 0; offset < sortedMemes.size(); offset+=memes.size())
 		{
 			std::copy(memes.begin(), memes.end(), sortedMemes.begin() + offset);
 		}
-		
-		// 2. Reverse sort on num
-		/*// Method 1 
+		*/
+		// Method 4 (Chili's solution)
+		auto sortedMemes = memes;
+		for (int i = 0; i < 3; i++)
+		{
+			sortedMemes.insert(sortedMemes.end(), memes.begin(), memes.end());
+		}
+
+		// STEP 2. Sort - 1st sorting run
+		// Method 1: Sort Major (by number descending) - use reverse iterators
+		/*
 		std::sort(sortedMemes.rbegin(), sortedMemes.rend());
 		*/
-		// Method 2
+		// Method 2: Sort Major (by number descending) - use Lambda function
+		/*
 		std::sort(sortedMemes.begin(), sortedMemes.end(),
 				[](Pube& lhs, Pube& rhs) {return lhs.num > rhs.num; }
 			);
-		
-		/*//Method 3: define own custom functor in the Pube class
+		*/
+		// Method 3: Sort Major (by number descending) - define own custom functor in the Pube class
+		/*
 		std::sort(sortedMemes.begin(), sortedMemes.end(), Pube::Sort1{} );
 		*/
-		/*// Method 4: define functor in situ
+		// Method 4: Sort Major (by number descending) - define functor in situ
+		/*
 		class Sort1
 		{
 		public:
@@ -304,8 +293,15 @@ int main()
 		};
 		std::sort(sortedMemes.begin(), sortedMemes.end(), Sort1{});
 		*/
+		// Method 5 (CHILI METHOD): Sort Minor (by meme string name ascenting)
+		std::sort(sortedMemes.begin(), sortedMemes.end(), [](const Pube& lhs, const Pube& rhs)
+			{
+				return lhs.str < rhs.str;
+			} );
 
-		// 3. 2nd level sort on meme name
+		// STEP 3. Sort - 2nd sorting run
+		// Method 1: Sort Minor (by meme string name ascenting)
+		/*
 		auto lambSort2 = [] (Pube& lhs, Pube& rhs) 
 		{ 
 			return lhs.str < rhs.str;
@@ -314,7 +310,6 @@ int main()
 		//auto begin2 = std::find_if(sortedMemes.begin(), sortedMemes.end(), [](const Pube& p)->bool {return (int(p) != 3); });
 		//std::sort(sortedMemes.begin(), begin2, Pube::Sort2{});
 		//std::sort(sortedMemes.begin(), end1+1, Pube::Sort2{});
-
 		auto iter_runner = sortedMemes.begin();
 		while (iter_runner != sortedMemes.end())
 		{
@@ -322,10 +317,23 @@ int main()
 			std::sort(iter_runner, end1 + 1, lambSort2);
 			iter_runner = end1+1;
 		}
+		*/
+		// Method 2 (CHILI METHOD): Sort Major (by number descending)
+		std::stable_sort(sortedMemes.begin(), sortedMemes.end(), [](const Pube& lhs, const Pube& rhs)
+			{
+				return lhs.num > rhs.num;
+			});
+
+		// STEP 4: Write to the console
+		// Method 1
+		/*
 		for (Pube& p : sortedMemes)
 		{
 			std::cout << p << std::endl;
 		}
+		*/
+		// Method 2 (CHILI METHOD)
+		std::copy(sortedMemes.begin(), sortedMemes.end(), std::ostream_iterator<Pube>(std::cout,"\n"));
 	}
 	std::cout << "============================================" << std::endl << std::endl;
 
